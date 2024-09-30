08c8a718c8167a8246a1697c6fd605b806275c52
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-09-30 18:37:27 +0530

    breaking news section, carousel, pagination

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "@/components/Header";
 import SearchBar from "@/components/SearchBar";
+import axios from "axios";
+import { NewsDataType } from "@/types";
+import BreakingNews from "@/components/BreakingNews";
 
 type Props = {};
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets();
+
+  const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getBreakingNews = async () => {
+    try {
+      const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=in&language=en&category=business&image=1&removeduplicate=1&size=5`;
+      const res = await axios.get(URL);
+      if (res && res.data) {
+        setBreakingNews(res.data.results);
+        setIsLoading(false);
+      }
+    } catch (error: any) {
+      console.log("Breaking news error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getBreakingNews();
+  }, []);
+  // ============================================================
   return (
     <View style={[styles.container, { paddingTop: safeTop }]}>
       <Header />
       <SearchBar />
+      {isLoading ? (
+        <ActivityIndicator size={"large"} />
+      ) : (
+        <BreakingNews newsList={breakingNews} />
+      )}
     </View>
   );
 };
